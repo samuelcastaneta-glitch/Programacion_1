b9bd27bf9691f8055157a72f854cf664a99c9838
samuelcastaneta-glitch <samuel.castaneta@ucb.edu.bo>
2025-08-25 19:26:27 -0400

    Update EJERCICIO_01_01.cpp

--- PRACTICA_01/EJERCICIO_01_01.cpp
+++ PRACTICA_01/EJERCICIO_01_01.cpp
@@ -1,8 +1,9 @@
 //el primer espiritu maquina del semestre
-#include <bits/std++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     cout << "El espiritu maquina surge" << endl;
     return 0;
+
 }
